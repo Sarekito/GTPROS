86ed7e6b982ecaa0a6399e0eea1d53c2e5bc2b81
Francisco <jfpp1994@gmail.com>
2016-01-24 18:40:16 +0100

    * Borrada la clase 'RolCat'. Ahora se utiliza el metodo 'getCategoria' de la enumeracion 'Rol' para obtener la categoria de un rol.

--- src/java/Controlador/Controlador.java
+++ src/java/Controlador/Controlador.java
@@ -12,7 +12,6 @@ import Proyecto.Dominio.TrabajadoresProyecto;
 import Trabajador.Despliegue.DespliegueTrabajadorLocal;
 import Trabajador.Dominio.Administrador;
 import Trabajador.Dominio.Rol;
-import Trabajador.Dominio.RolCat;
 import Trabajador.Dominio.Trabajador;
 import Trabajador.Dominio.Vacaciones;
 import java.io.IOException;
@@ -380,7 +379,7 @@ public class Controlador extends HttpServlet {
     }
 
     private String registroTrabajador(HttpServletRequest request) {
-        Trabajador tr = new Trabajador(request.getParameter("usuario"), request.getParameter("clave"), Rol.get(request.getParameter("rol")), RolCat.dameCat(Rol.get(request.getParameter("rol"))));
+        Trabajador tr = new Trabajador(request.getParameter("usuario"), request.getParameter("clave"), Rol.get(request.getParameter("rol")), Rol.get(request.getParameter("rol")).getCategoria());
         boolean existe = despliegueTrabajador.buscaTrabajador(tr.getUser());
         if (!existe) {
             despliegueTrabajador.registrarTrabajador(tr);
--- src/java/Trabajador/Dominio/Rol.java
+++ src/java/Trabajador/Dominio/Rol.java
@@ -5,13 +5,23 @@ package Trabajador.Dominio;
  * @author antonio
  */
 public enum Rol {
-    JefeProyecto,
-    Analista,
-    Disenador,
-    AnalistaProgramador,
-    ResponsablePruebas,
-    Programador,
-    Probador;
+    JefeProyecto(Categoria.Uno),
+    Analista(Categoria.Dos),
+    Disenador(Categoria.Tres),
+    AnalistaProgramador(Categoria.Tres),
+    ResponsablePruebas(Categoria.Tres),
+    Programador(Categoria.Cuatro),
+    Probador(Categoria.Cuatro);
+    
+    private final Categoria categoria;
+    
+    public Categoria getCategoria() {
+        return categoria;
+    }
+    
+    private Rol(Categoria categoria) {
+        this.categoria = categoria;
+    }
     
     public static Rol get(String rol) {
         for (Rol r: values()) {
